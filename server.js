e37b8b9987233e856f16e58508a11e98c1b1df77
Aswad Ali <aswadgemini11@gmail.com>
2020-03-22 16:47:34 +0500

    Added express server and prepped project for heroku deployment

---
+++ server.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port: ${PORT}!`);
+});
