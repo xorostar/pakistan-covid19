d7200a87d03f86d25e5c2d7afc8e12e270eee11e
Aswad Ali <aswadgemini11@gmail.com>
2020-03-26 04:38:47 +0500

    Enhanced responsiveness on < 500px screens

--- client/src/components/layouts/Header.js
+++ client/src/components/layouts/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import pakistanFlag from '../../images/pakistan-flag.jpg';
 
 const Header = () => {
@@ -7,13 +7,15 @@ const Header = () => {
     <header>
       <div className='left'>
         <h1>
-          <img
-            src={pakistanFlag}
-            width='75px'
-            style={{ marginRight: '10px' }}
-            alt=''
-          />
-          <span>PAKISTAN COVID 19</span>
+          <Link to='/' style={{ textDecoration: 'none' }}>
+            <img
+              src={pakistanFlag}
+              width='75px'
+              style={{ marginRight: '10px' }}
+              alt=''
+            />
+            <span>PAKISTAN COVID 19</span>
+          </Link>
         </h1>
       </div>
       <div className='right'>
